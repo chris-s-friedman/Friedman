b0cb23ee6635fea8f2676f987cb0510181e736a7
Chris Friedman <csf32@drexel.edu>
2018-04-09 12:51:58 -0400

    save analytical data sets

---
+++ R/save_ana.R
@@ -0,0 +1,21 @@
+#' Save Analytical Data
+#'
+#' Saves analytical data in all the forms used by the Hassrick Lab.
+#'
+#' @param x data frame, tibble, some data
+#' @param file_name location to save to (including the file name). Do not
+#'   include the file extension. That is handled internally.
+#'
+#' @importFrom glue glue
+#' @importFrom readr write_csv
+#' @importFrom haven write_sav wrote_sas write_dta
+#'
+#' @return NULL invisibly
+#' @export
+save_ana <- function(x, file_name) {
+  readr::write_csv(x, path = glue(file_name, ".csv"))
+  haven::write_sav(x, path = glue(file_name, ".sav"))
+  haven::write_sas(x, path = glue(file_name, ".sas7bdat"))
+  haven::write_dta(x, path = glue(file_name, ".dta"))
+  saveRDS(x, file = glue(file_name, ".Rds"))
+}
