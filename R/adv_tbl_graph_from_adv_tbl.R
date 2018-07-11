bdbde973616b8de90867fc299de67ad97fca40bb
Chris Friedman <csf32@drexel.edu>
2018-07-11 11:17:54 -0400

    first arg was the name of a function.
    That's a bad idea for debugging

--- R/adv_tbl_graph_from_adv_tbl.R
+++ R/adv_tbl_graph_from_adv_tbl.R
@@ -1,15 +1,15 @@
-#' Convert adv_tbl in an igraph object
+#' Convert adv_data in an igraph object
 #'
 #' Converts advanced format network data (a la ORA) into an igraph object
 #'
-#' @param adv_tbl adv_tbl data created by \code{\link{adv_tbl()}}
+#' @param adv_data adv_tbl data created by \code{\link{adv_tbl()}}
 #'
 #' @return an igraph graph object
 #'
 #' @author Chris Friedman, \email{chris.s.friedman@@gmail.com}
 #' @seealso
 #'
-#' @examples graph_from_adv_tbl(adv_tbl)
+#' @examples graph_from_adv_tbl(adv_data)
 #'
 #' @import  dplyr
 #' @importFrom tidyr spread
@@ -17,19 +17,19 @@
 #'
 #' @export
 #'
-graph_from_adv_tbl <- function(adv_tbl){
-  if(!"adv_tbl" %in% class(adv_tbl)) {
-    stop("Input is not an adv_tbl")
+graph_from_adv_tbl <- function(adv_data){
+  if(!"adv_tbl" %in% class(adv_data)) {
+    stop("Input is not an adv_data")
   }
   # create the edge list
-  adv_edge <- as_adv_edge(adv_tbl)
+  adv_edge <- as_adv_edge(adv_data)
   # create the vertex list
-  adv_vert <- as_adv_attr(adv_tbl) %>%
     select(-attr_type) %>%
     spread(attr_name, attr_value) %>%
+  adv_vert <- as_adv_attr(adv_data)
     select(node_name, everything())
   # make the igraph object
   graph_from_data_frame(d = adv_edge,
-                                directed = TRUE,
-                                vertices = adv_vert)
+                        directed = TRUE,
+                        vertices = adv_vert)
 }
